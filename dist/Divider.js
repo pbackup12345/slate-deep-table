2f6d3ea7e41d049452b6e74c2da61d8230c9e3d0
Peter Szeman <pbackup12345@gmail.com>
2019-07-22 10:06:01 +0200

    Fix

--- dist/Divider.js
+++ dist/Divider.js
@@ -28,6 +28,14 @@ var style = {
   zIndex: 2
 };
 
+var spanStyle = {
+  top: 0,
+  right: -3,
+  position: "absolute",
+  userSelect: "none",
+  zIndex: 3
+};
+
 var Divider = function Divider(props) {
   var onMouseDown = function onMouseDown(e) {
     e.persist();
@@ -83,13 +91,19 @@ var Divider = function Divider(props) {
   return _react2.default.createElement(
     _react2.default.Fragment,
     null,
-    _react2.default.createElement("div", {
-      style: style,
-      onMouseDown: onMouseDown,
-      onMouseOver: onMouseOver,
-      onMouseOut: onMouseOut,
-      onClick: onClick
-    })
+    _react2.default.createElement(
+      "div",
+      {
+        style: style,
+        onMouseDown: onMouseDown,
+        onMouseOver: onMouseOver,
+        onMouseOut: onMouseOut },
+      _react2.default.createElement(
+        "span",
+        { onClick: onClick, style: spanStyle },
+        "p"
+      )
+    )
   );
 };
 
